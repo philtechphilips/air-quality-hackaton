a6e34733f38d35d52b7a1d1d50f1d4b87f840667
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2023-11-24 07:20:41 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/HomePage"
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-   <Routes>
-    <Route path="/" element={<HomePage />}></Route>
-    <Route path="*" element="404 Page"></Route>
-    <Route path="/dashboard" element="Dshboard Page"></Route>
-   </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="*" element="404 Page"></Route>
+        <Route path="/dashboard" element="Dashboard Page"></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
